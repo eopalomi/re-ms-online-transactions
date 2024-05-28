b49437502c60f512e3fff496c5ab769e0af21a3b
JAimeenestarees321 <jaimenestares1012@gmail.com>
2024-05-27 19:52:41 -0500

    [Fix] name event change

--- src/modules/payment-process/application/uses-cases/create-payment.use-case.ts
+++ src/modules/payment-process/application/uses-cases/create-payment.use-case.ts
@@ -1,17 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { PaymentRepository } from '../../domain/repository/payment.repository';
 import { RabbitMQService } from '@src/libs/rabbit-mq/rabbitmq.service';
 
 @Injectable()
 export class CreatePaymentUseCase {
-  constructor( 
-    @Inject("TRANSACTION_PUBLISHER")
+  constructor(
+    @Inject('TRANSACTION_PUBLISHER')
     private readonly transactionPublisher: RabbitMQService,
   ) {}
 
   public async execute(registrationRequestDto: any) {
     await this.transactionPublisher.sendToQueue(
-      'trx.cofide',
+      'trasaction.event',
       'trx.cofide',
       registrationRequestDto,
     );
--- src/modules/payment-process/presentation/controllers/process-payment.controller.ts
+++ src/modules/payment-process/presentation/controllers/process-payment.controller.ts
@@ -1,16 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreatePaymentUseCase } from '../../application/uses-cases/create-payment.use-case';
 
 @Controller('/transaction')
 export class PaymentProcessController {
-  constructor(
-    private readonly createPaymentUseCase: CreatePaymentUseCase,
-  ) {}
+  constructor(private readonly createPaymentUseCase: CreatePaymentUseCase) {}
 
   @Post('')
-  async registrationRequest(
-    @Body() data: any,
-  ) {
+  async registrationRequest(@Body() data: any) {
     this.createPaymentUseCase.execute(data);
     return { responseCode: '00', message: '' };
   }
